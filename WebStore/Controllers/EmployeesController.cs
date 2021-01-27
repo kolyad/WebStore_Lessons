4bcfdf8d00b1586736c389c7a4bf9e1d9ff1b17e
kolyad <kolyad@mail.ru>
2021-01-27 22:57:58 +0300

    added edit Employee feature

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using WebStore.Data;
 using WebStore.Infrastructure.Interfaces;
 using WebStore.Models;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -26,6 +28,53 @@ namespace WebStore.Controllers
                 return View(model);
 
             return NotFound();
+        }
+
+        public IActionResult Edit(int Id)
+        {
+            if (Id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var employee = _employeesData.Get(Id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+
+            return View(new EmployeeViewModel
+            {
+                Id = employee.Id,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                MiddleName = employee.Patronymic,
+                BirthDate = employee.BirthDate,
+                City = employee.City,
+                HireDate = employee.HireDate
+            });
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel model)
+        {
+            _ = model ?? throw new ArgumentNullException();
+
+            var employee = new Employee
+            {
+                Id = model.Id,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Patronymic = model.MiddleName,
+                BirthDate = model.BirthDate,
+                City = model.City,
+                HireDate = model.HireDate
+            };
+
+            _employeesData.Update(employee);
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ WebStore/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        /// <summary>
+        /// Id сотрудника
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Фамилия сотрудника
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Имя сотрудника
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Отчество сотрудника
+        /// </summary>
+        public string MiddleName { get; set; }
+        /// <summary>
+        /// Дата рождения сотрудника
+        /// </summary>
+        public DateTime BirthDate { get; set; }
+        /// <summary>
+        /// Возраст сотрудника
+        /// </summary>
+        public int Age
+        {
+            get
+            {
+                var now = DateTime.Now;
+
+                var age = now.Year - BirthDate.Year;
+
+                if (age <= 0)
+                {
+                    return 0;
+                }
+
+                if ((now.Month < BirthDate.Month) ||
+                    (now.Month == BirthDate.Month && now.Day < BirthDate.Day))
+                {
+                    age--;
+                }
+
+                return age;
+            }
+        }
+        /// <summary>
+        /// Дата найма сотрудника
+        /// </summary>
+        public DateTime HireDate { get; set; }
+        /// <summary>
+        /// Город проживания сотрудинка
+        /// </summary>
+        public string City { get; set; }
+    }
+}
