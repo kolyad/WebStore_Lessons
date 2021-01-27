4ac270292778c77fcad4798cba1e23079110b593
kolyad <kolyad@mail.ru>
2021-01-27 21:48:46 +0300

    check codeapage of Startup.cs

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -30,6 +29,8 @@ namespace WebStore
 
             app.UseEndpoints(endpoints =>
             {
+                // Русский комментарий
+                // Русский комментарий
                 endpoints.MapControllerRoute(
                     "default",
                     "{controller=Home}/{action=Index}/{Id?}");
