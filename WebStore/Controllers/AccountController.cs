ef338c3f2070e8b42d01d418e09edb9005dc4deb
kolyad <kolyad@mail.ru>
2021-02-04 23:33:06 +0300

    add controller Account and user registration

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -5,20 +5,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Domain.Entities.Identity;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
     public class AccountController : Controller
     {
         private readonly UserManager<User> _userManager;
-        private readonly RoleManager<Role> _roleManager;
+        private readonly SignInManager<User> _signInManager;
 
         public AccountController(
             UserManager<User> userManager,
-            RoleManager<Role> roleManager)
+            SignInManager<User> signInManager)
         {
             _userManager = userManager;
-            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register() => View(new RegisterUserViewModel());
+
+        [HttpPost, ValidateAntiForgeryToken, ActionName("Register")]
+        public async Task<IActionResult> RegisterAsync(RegisterUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var user = new User
+            {
+                UserName = model.UserName                
+            };
+
+            var registrationResult = await _userManager.CreateAsync(user, model.Password);
+            if (registrationResult.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in registrationResult.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View(model);
         }
     }
 }
---
+++ WebStore/ViewModels/RegisterUserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.ViewModels
+{
+    public class RegisterUserViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение пароля")]
+        [Compare(nameof(Password))]
+        public string PasswordConfirm { get; set; }
+    }
+}
