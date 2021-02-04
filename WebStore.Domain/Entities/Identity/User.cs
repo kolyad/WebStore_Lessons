690979783ca8beeeed82d62f61edea2ce4606674
kolyad <kolyad@mail.ru>
2021-02-04 22:09:46 +0300

    add base identity classes

---
+++ WebStore.Domain/Entities/Identity/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Domain.Entities.Identity
+{
+    public class User : IdentityUser
+    {
+        public const string Administrator = "Admin";
+
+        public const string DefaultAdministratorPassword = "Admin";
+
+        public string Description { get; set; }
+    } 
+}
