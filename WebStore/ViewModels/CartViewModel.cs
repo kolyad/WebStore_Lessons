3e08a34d44c0fe28ab1e79439fdc0ca8ea1f1ef5
kolyad <kolyad@mail.ru>
2021-02-08 23:29:46 +0300

    add Cart models and view-model

---
+++ WebStore/ViewModels/CartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WebStore.ViewModels
+{
+    public class CartViewModel
+    {
+        public IEnumerable<(ProductViewModel Product, int Quantity)> Items { get; set; }
+
+        public int ItemsCount => Items?.Sum(x => x.Quantity) ?? 0;
+
+        public decimal TotalPrice => Items?.Sum(x => x.Quantity * x.Product.Price) ?? 0m;
+    }
+}
