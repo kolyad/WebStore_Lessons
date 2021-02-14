28a1e17a5e23ce67969dbddd26d6e1d9e7588368
kolyad <kolyad@mail.ru>
2021-02-14 12:28:05 +0300

    add admin page infrastructure and initial content

--- WebStore/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -5,7 +5,7 @@ using WebStore.Domain.Entities.Identity;
 namespace WebStore.Areas.Admin.Controllers
 {
     [Area("Admin"), Authorize(Roles = Role.Administrator)]
-    public class HomeController : Controller
+    public class ProductsController : Controller
     {
         public IActionResult Index()
         {
