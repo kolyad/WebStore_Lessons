ae7cb7d1e7642b40d878254b720712532aad960f
kolyad <kolyad@mail.ru>
2021-02-14 23:05:12 +0300

    add Order entities

---
+++ WebStore.Domain/Entities/Orders/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Domain.Entities.Orders
+{
+    public class Order : NamedEntity
+    {
+        [Required]
+        public User User { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public DateTime DateOrder { get; set; } = DateTime.Now;
+
+        public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();
+    }
+}
---
+++ WebStore.Domain/Entities/Orders/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebStore.Domain.Entities.Orders
+{
+    public class OrderItem
+    {
+        [Required]
+        public Order Order { get; set; }
+
+        [Required]
+        public Product Product { get; set; }
+
+        [Column(TypeName="decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        [NotMapped]
+        public decimal TotalItemPrice => Price * Quantity;
+    }
+}
