f074f7095003a8e266c22682bb5782b071afffbf
kolyad <kolyad@mail.ru>
2021-04-19 23:46:31 +0300

    add WebAPI controller and view in base app

---
+++ UI/WebStore/Controllers/WebAPIController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Interfaces.TestAPI;
+
+namespace WebStore.Controllers
+{
+    public class WebAPIController : Controller
+    {
+        private readonly IValuesService _valuesService;
+
+        public WebAPIController(IValuesService valuesService)
+        {
+            _valuesService = valuesService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _valuesService.Get();
+            return View(values);
+        }
+    }
+}
