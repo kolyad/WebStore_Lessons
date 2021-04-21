eccb5b0b4495e378e69820982b2dcc826ca556bd
kolyad <kolyad@mail.ru>
2021-04-21 22:11:52 +0300

    added Dto models

---
+++ Common/WebStore.Domain/Dto/BrandDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WebStore.Domain.Dto
+{
+    public class BrandDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Order { get; set; }
+    }
+}
---
+++ Common/WebStore.Domain/Dto/SectionDto.cs
@@ -0,0 +1,13 @@
+﻿namespace WebStore.Domain.Dto
+{
+    public class SectionDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Order { get; set; }
+
+        public int? ParentId { get; set; }
+    }
+}
