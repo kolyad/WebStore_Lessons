cd02a51fc02c9e8a3fd39a8f89fdc73a88ce520e
kolyad <kolyad@mail.ru>
2021-04-25 22:35:22 +0300

    Identity controllers draft

---
+++ Services/WebStore.ServiceHosting/Controllers/Identity/RolesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces;
+
+namespace WebStore.ServiceHosting.Controllers.Identity
+{
+    [Route(WebApi.Identity.Role)]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly RoleStore<Role> _roleStore;
+
+        public RolesController(WebStoreDb db)
+        {
+            _roleStore = new RoleStore<Role>(db);
+        }
+    }
+}
---
+++ Services/WebStore.ServiceHosting/Controllers/Identity/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces;
+
+namespace WebStore.ServiceHosting.Controllers.Identity
+{
+    [Route(WebApi.Identity.User)]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserStore<User, Role, WebStoreDb> _userStore;
+
+        public UsersController(WebStoreDb db)
+        {
+            _userStore = new UserStore<User, Role, WebStoreDb>(db);                    
+        }
+    }
+}
